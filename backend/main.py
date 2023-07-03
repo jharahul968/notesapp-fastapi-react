f980730fcf920c400f544ac736387ef338598cc8
jharahul968 <jharahul968@gmail.com>
2023-07-03 21:40:25 +0545

    fixed editing error

--- backend/main.py
+++ backend/main.py
@@ -30,20 +30,20 @@ def addNotes(data=Body()):
                              get_attributes=['*'])
     return notes
 
-@app.get('notes/{pk}')
+@app.get('/notes/{pk}')
 def getNote(pk:str):
     notes=db.search_by_hash('notesapp', 'notes', [pk],
                             get_attributes=['*'])
     return notes[0]
 
-@app.put('notes/{id}')
+@app.put('/notes/{id}')
 def updateNote(id:str, data=Body()):
     db.update('notesapp', 'notes', [{'id':id, 'body':data['body']}])
     notes=db.search_by_value('notesapp', 'notes', 'id', '*',
                              get_attributes=['*'])
     return notes
 
-@app.delete('notes/{id}')
+@app.delete('/notes/{id}')
 def deleteNote(id:str):
     db.delete('notesapp', 'notes', [id])
     notes=db.search_by_value('notesapp', 'notes', 'id', '*',
